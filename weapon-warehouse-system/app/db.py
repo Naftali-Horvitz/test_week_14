be8811dd446a36780e8d69d811a15604f2485ca7
Naftali Horviz <n0548498093@gmail.com>
2026-01-22 11:49:28 +0200

    create app

---
+++ weapon-warehouse-system/app/db.py
@@ -0,0 +1,59 @@
+import mysql.connector
+import os
+import pandas as pd
+
+
+db_host = os.getenv('DB_HOST', 'localhost')
+db_user = os.getenv('DB_USER', 'root')
+db_password = os.getenv('DB_PASSWORD')
+db_name = os.getenv('DB_NAME')
+
+def get_conn():
+    return mysql.connector.connect(
+    host=db_host,
+    user=db_user,
+    password=db_password,
+    database=db_name
+)
+
+def create_table(conn):
+    cursor = conn.cursor()
+    try:
+        create_table_query = """
+        CREATE TABLE IF NOT EXISTS weapons (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            weapon_id VARCHAR(255),
+            weapon_name: VARCHAR(255),
+            weapon_type: VARCHAR(255), 
+            range_km: INT
+            weight_kg: FLOAT
+            manufacturer: VARCHAR(255),
+            origin_country: VARCHAR(255),
+            storage_location: VARCHAR(255), 
+            year_estimated: INT,
+            risk_level
+        )
+        """
+        cursor.execute(create_table_query)
+        conn.commit() 
+        print("Table created successfully!")
+    except Exception as e:
+        raise e
+
+
+def insert_data(data: list[dict], conn):
+    insert_data_query = """
+    INSERT INTO weapons (weapon_id, weapon_name, weapon_type, range_km, 
+    weight_kg, manufacturer, origin_country, storage_location, year_estimated, risk_level)
+    VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s);
+    """
+    data = data
+    cursor = conn.cursor()
+    for i in data:
+        cursor.executemany(insert_data_query, tuple(i.values()))
+    return {
+    "status": "success",
+    "inserted_records": cursor.rowcount
+    }
+
+    
---
+++ weapon-warehouse-system/app/main.py
@@ -0,0 +1,44 @@
+from fastapi import FastAPI, UploadFile, HTTPException
+import uvicorn
+import pandas as pd
+import numpy as np
+from db import get_conn, create_table, insert_data
+
+app = FastAPI()
+
+def add_risk_level_col(data):
+        data['risk_level'] = pd.cut(
+            data['range_km'],
+            bins=[0, 20, 100, 300, np.inf],
+            labels=["low", "mediume", "high", "extreme"]
+        )
+
+
+def handling_manufacturer_col(data: pd.DataFrame):
+    data['manufacturer'] = data['manufacturer'].fillna("Unknown")
+
+@app.post("/upload")
+def upload_file(file: UploadFile):
+    # if file
+    data = pd.read_csv(file.file)
+    add_risk_level_col(data=data)
+    handling_manufacturer_col(data=data)
+    try:
+        conn = get_conn()
+    except HTTPException as e:
+        raise {"msg": f"Connection failed {e}"}
+    try:
+        create_table(conn)
+    except HTTPException as e:
+        raise {"msg": f"create table failed {e}"}
+    
+    try:
+        return insert_data(data.to_dict("records"), conn)
+    except HTTPException as e:
+        raise {"msg": f"insert data failed {e}"}
+
+
+
+if __name__ =="__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8001, reload=True)
+
